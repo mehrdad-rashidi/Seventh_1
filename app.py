ff5084ac189934ab73e738c9337a60c8fa0ddacd
Mehrdad Rahidi <rashidi.mehrdad@gmail.cm>
2024-01-11 08:24:47 +0330

    Forth commit

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from flask_jwt import JWT
 
 from security import authenticate, identity
 from resources.userRegister import UserRegister
-from resources.item import Item
+from resources.retrieveItem import Item
 from resources.itemList import ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.debug = True
 app.secret_key = 'jose'
 api = Api(app)
 
@@ -17,7 +19,9 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    from orm.db import db
+    db.inite_app(app)
+    app.run(debug=True, port=5000)
 
 # virtualenv venv --python=python3.11   --create virtual environment
 # source venv/bin/activate
